6fcc453a7a019befd39d92326efdce985560ea15
pbaffiliatetwoop <pbaffiliate2@gmail.com>
2016-10-14 06:42:49 +0300

    Commit from Test -  vessel/defaultAclsScriptTest2.js

---
+++ vessel/defaultAclsScriptTest2.js
@@ -0,0 +1,10 @@
+<script>
+<scriptACL>
+<execute>authenticated</execute>
+<read>authenticated</read>
+<write>authenticated</write>
+</scriptACL>
+<code> 
+<![CDATA[return "heavy metal will not die";]]>
+</code>
+</script>
