7ac5f1a941b03a2958042164f6d91ad3309d38a8
pbaffiliatetwoop <10269519+pbaffiliatetwoop@users.noreply.github.com>
2019-05-13 11:55:52 +0300

    This is a test commit message

--- checkoutMetadataScript1.js
+++ checkoutMetadataScript1.js
@@ -1,5 +1,1 @@
-/** Script ACLs do not delete 
- read=authenticated 
-write=nobody
-execute=authenticated
- **/systemLogger.setLevel("off"); return "temple of the king";
+systemLogger.setLevel("off"); return "temple of the king";
